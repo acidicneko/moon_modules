1fb6f92f1bc248a01d8c9b97491cab63ea83364d
Ayush Yadav <57079688+ayush7788@users.noreply.github.com>
2021-06-13 20:22:28 +0530

    Create basics.sh

---
+++ modules/basics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+username () {
+	echo "\e[${USERNAME_COL}m\u\e[0m"
+}
+
+hostname () {
+	echo "\e[${HOSTNAME_COL}m\h\e[0m"
+}
+
+working_dir () {
+	echo "\e[${WORKING_DIR_COL}m\w\e[0m"
+}
+
