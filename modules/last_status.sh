7d51a6bdb141147ac9a3a213130ef93921a2846c
ayush7788 <ayush7788899@gmail.com>
2021-06-14 12:33:57 +0530

    adding modules

---
+++ modules/last_status.sh
@@ -0,0 +1,16 @@
+check_last_status_main () {
+	exit_code=$?
+	if [[ $exit_code != 0 ]] ; then
+		status=""
+		[[ "$SHOW_EXIT_CODE" == 1 ]] &&
+			status="${status} $exit_code"
+		echo "$status"
+		return 1
+	fi
+	echo " "
+	return 0
+}
+
+check_last_status () {
+	echo "\$(check_last_status_main)"
+}
